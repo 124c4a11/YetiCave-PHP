03cfa2467df2a6cbe5bef67313b1eb6a88370d8f
124c4a11 <124c4a11@mail.ru>
2018-06-10 15:26:39 +0300

    function price_format

--- index.php
+++ index.php
@@ -38,6 +38,13 @@ $lots = [
         'image' => 'img/lot-6.jpg'
     ],
 ];
+
+function format_price($price) {
+    $price = number_format($price, 0, '', ' ');
+    return $price . ' <b class="rub">р</b>';
+}
+
+format_price(10999)
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -121,7 +128,7 @@ $lots = [
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$lot['price']; ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?=format_price($lot['price']); ?></span>
                             </div>
                             <div class="lot__timer timer">
 

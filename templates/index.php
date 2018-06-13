561e1aac28db493871eef7275d7310e647c724ab
124c4a11 <124c4a11@mail.ru>
2018-06-13 19:11:14 +0300

    lots__item: htmlspecialchars for user data

--- templates/index.php
+++ templates/index.php
@@ -33,8 +33,8 @@
 					<img src=<?=$lot['image']; ?> width="350" height="260" alt="Сноуборд">
 				</div>
 				<div class="lot__info">
-					<span class="lot__category"><?=$lot['category']; ?></span>
-					<h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['title']; ?></a></h3>
+					<span class="lot__category"><?=htmlspecialchars($lot['category']); ?></span>
+					<h3 class="lot__title"><a class="text-link" href="lot.html"><?=htmlspecialchars($lot['title']); ?></a></h3>
 					<div class="lot__state">
 						<div class="lot__rate">
 							<span class="lot__amount">Стартовая цена</span>
